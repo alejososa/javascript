cfbce91e182f3e53d59eeccc56c7da2a08ec6002
Alejo Sosa <alejososa1987@gmail.com>
2022-04-23 00:14:56 -0300

    tipo de actividad, dias y pago

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,5 @@
 //sistema de cobro por mensualidad en una academia/gimnasio
-
-//tipos de cuotas y precios
-
-var cantidadDeDias = ("3" , "libre"); 
-var precios = (3000 , 4000);
+ 
 const iva = 21;
 const actividad = ("bjj" , "muay thai" , "mma");
 
@@ -17,7 +13,8 @@ do {
  } while (tipoDeActividad != "BJJ" || tipoDeActividad != "MUAY THAI" || (tipoDeActividad != "MMA"));
 
 
-var cantidadDeDias = prompt ("ingrese cantidad de dias");
+let cantidadDeDias = prompt ("ingrese cantidad de dias");
+
 let formaDePago;
 do {
     formaDePago = prompt ("ingrese metodo de pago (efectivo/tarjeta)").toUpperCase();
@@ -26,6 +23,7 @@ do {
     }
 
  } while (formaDePago != "EFECTIVO" || formaDePago != "TARJETA");
+
 
 console.log ("tipoDeActividad" + tipoDeActividad);
 console.log ("cantidadDeDias" + cantidadDeDias);
