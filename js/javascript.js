691d9af582eecf0c706f939e8cf8d6eb2c2f4336
Alejo Sosa <alejososa1987@gmail.com>
2022-04-23 15:10:35 -0300

    agregada la opcion libre

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,32 @@
 //sistema de cobro por mensualidad en una academia/gimnasio
  
-const actividad = ("bjj" , "muay thai" , "mma");
-var precioPorTres = 3000;
-var precioMasDeTres = 4000;
 /*datos del socio, a futuro la idea es pedir nombre, que se pueda anotar y en caso de estar anotado que lo busque en un determinado grupo, y asi ver si esta la cuota paga o no.
 si esta paga, sigue todo normal
 si esta cerca de finalizar, que de un aviso y mande mail
 si esta impaga, que de las opciones de pago 
 */
+const actividad = ("bjj" , "muay thai" , "mma");
+
+var precioPorTres = 3000;
+var precioMasDeTres = 4000;
+var precioLibre = 5000; 
 
 let nombreDelSocio = prompt ("ingrese su nombre");
 
   let tipoDeActividad;
 do {
-    tipoDeActividad = prompt ("ingrese su actividad (bjj/muay thai/mma)").toUpperCase();
-    if ((tipoDeActividad == "BJJ") || (tipoDeActividad == "MUAY THAI") || (tipoDeActividad == "MMA")) {
+    tipoDeActividad = prompt ("ingrese su actividad (bjj/muay thai/mma/libre)").toUpperCase();
+    if ((tipoDeActividad == "BJJ") || (tipoDeActividad == "MUAY THAI") || (tipoDeActividad == "MMA") || (tipoDeActividad == "LIBRE")) {
         break;
     }
- } while (tipoDeActividad != "BJJ" || tipoDeActividad != "MUAY THAI" || (tipoDeActividad != "MMA"));
+ } while (tipoDeActividad != "BJJ" || tipoDeActividad != "MUAY THAI" || (tipoDeActividad != "MMA")|| (tipoDeActividad != "LIBRE"));
 
 
 let cantidadDeClases = prompt ("ingrese cantidad de clases por semana");
 let precioFinal;
 if (cantidadDeClases <=3) {precioFinal = precioPorTres} 
 if (cantidadDeClases >3) {precioFinal = precioMasDeTres};
-
+if (tipoDeActividad = "LIBRE") {precioFinal = precioLibre};
 
 let formaDePago;
 do {
@@ -34,6 +36,8 @@ do {
     }
 
  } while (formaDePago != "EFECTIVO" || formaDePago != "TARJETA");
+
+
 
 
 console.log ("Nombre del socio " +  nombreDelSocio);
